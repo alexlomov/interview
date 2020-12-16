53c9f9184609b2c77d62cd7185b83337b9e26690
Alexander Lomov <alexander.lomov@me.com>
2020-12-16 02:05:19 +0100

    Add successful scenario test for FraudClient.

--- src/test/scala/com/generic/bank/fraud/client/DefaultFraudClientSpec.scala
+++ src/test/scala/com/generic/bank/fraud/client/DefaultFraudClientSpec.scala
@@ -12,13 +12,14 @@ import scala.concurrent.{ ExecutionContext, Future }
 
 class DefaultFraudClientSpec extends AsyncFunSpec with should.Matchers {
 
+  val finMsg = FinancialMessage(
+    SenderBic(Bic("senderBic")),
+    ReceiverBic(Bic("receiverBic")),
+    Amount(Value(110d), Currency.SGD)
+  )
+
   describe("Fraud API domain error translation") {
 
-    val finMsg = FinancialMessage(
-      SenderBic(Bic("senderBic")),
-      ReceiverBic(Bic("receiverBic")),
-      Amount(Value(110d), Currency.SGD)
-    )
     it("translates Illegal error of Fraud API into Illegal error of the client domain") {
       val IllegalText = "It is illegal to pay with pig tails nowadays!"
       val illegalCall: FinancialMessage => Future[Either[api.Error.Illegal, api.domain.FraudResult]] =
@@ -50,6 +51,49 @@ class DefaultFraudClientSpec extends AsyncFunSpec with should.Matchers {
       }
     }
 
+    it("handles client side error of Fraud Client as ClientFailure error") {
+
+      val failedCall: FinancialMessage => Future[Either[api.Error.System, api.domain.FraudResult]] =
+        _ => {
+          Future.failed(new RuntimeException("The FraudClient has failed"))
+        }
+
+      new DefaultFraudClient(failedCall)(ExecutionContext.global).call(
+        finMsg
+      ).map {
+        case Left(Error.ClientFailure(_)) => succeed
+        case x                            => fail(s"$x is unexpected in this test")
+      }
+    }
+  }
+
+  describe("Fraud API domain FraudResult translation") {
+
+    it("adopts Fraud API FraudResult.Fraud into the client domain ") {
+      val successfulCall: FinancialMessage => Future[Either[api.Error, api.domain.FraudResult]] =
+        _ => Future.successful(api.domain.FraudResult.Fraud.asRight)
+
+      new DefaultFraudClient(successfulCall)(ExecutionContext.global).call(
+        finMsg
+      ).map {
+        case Right(domain.FraudResult.Fraud) => succeed
+        case x                               => fail(s"$x is unexpected in this test")
+      }
+
+    }
+
+    it("adopts Fraud API FraudResult.NoFraud into the client domain ") {
+      val successfulCall: FinancialMessage => Future[Either[api.Error, api.domain.FraudResult]] =
+        _ => Future.successful(api.domain.FraudResult.NoFraud.asRight)
+
+      new DefaultFraudClient(successfulCall)(ExecutionContext.global).call(
+        finMsg
+      ).map {
+        case Right(domain.FraudResult.NoFraud) => succeed
+        case x                                 => fail(s"$x is unexpected in this test")
+      }
+
+    }
   }
 
 }
